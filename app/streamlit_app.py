9ff3c1918150b8b5efa55d5400ea9f26c806e35b
janjarco99 <janjarco99@gmail.com>
2023-10-22 01:12:58 +0200

    First scatter

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,12 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
+# %% Data import
+# --------------------
 data = pd.read_csv('../data/activities/processed/Activities_Garmin.csv')
+# data filter if Avg HR > 0
+data = data[data['Avg HR'] > 0]
 
 
+# %% Header
+# --------------------
 st.header("Garmin Data Dashboard: Jan Jarco activities")
 
+
+# %% Scatter plot
+# --------------------
 # scatter plot with variables to choose from dataset
 x = st.selectbox('X axis', data.columns)
 y = st.selectbox('Y axis', data.columns)
